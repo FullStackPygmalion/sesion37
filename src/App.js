0b7e6d08f8eb3635276928747ed645b58fd959d0
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-15 21:14:51 -0500

    React context classes and hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ClassContext from './pages/classContext'
+import HookContext from './pages/hookContext'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <HookContext />
+      // <ClassContext />
+  )
 }
 
 export default App;
